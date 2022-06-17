0b424ad2099bcb0ee1d71690ee4dc43372fc6813
RomainC75 <rom.chenard@gmail.com>
2022-06-17 15:08:34 +0200

    final ?

--- routes/swap.route.js
+++ routes/swap.route.js
@@ -3,28 +3,28 @@ const authentication = require("../middleware/authentication.mid")
 const House = require("../models/House.model")
 const router = require("express").Router()
 const User = require('../models/User.model')
+const {SchemaTypes} = require('mongoose')
+
 //get a swap between 2 houses
 //verify H1 is owned by the user
 router.get("/", authentication, async(req, res, next) => {
-    try {
-        
-        const user = await User.findById(req.user._id)
-        const swapFound = await Swap.find({
-            $or:[
-                {$and:[ {$house1:req.body.house1},{$house2:req.body.house2} ]},
-                {$and:[ {$house1:req.body.house2},{$house2:req.body.house1} ]}
-            ]
-        }).populate('house1').populate('house2')
-
-        res.status(200).json(swapFound)
-
-      } catch (err) {
-        next(err)
-      }
-    })
+  try {
+      const user = await User.findById(req.user._id)
+      const swapFound = await Swap.find({
+          $or:[
+              {$and:[ {$house1:req.body.house1},{$house2:req.body.house2} ]},
+              {$and:[ {$house1:req.body.house2},{$house2:req.body.house1} ]}
+          ]
+      }).populate('house1').populate('house2')
+      res.status(200).json(swapFound)
+  } catch (err) {
+    next(err)
+  }
+})
 
 //create a new swap
 router.post("/", authentication, async (req, res, next) => {
+  console.log( req.body )
   try {
     if(!req.body.house1){
         res.status(400).json({message:"missing house1!"})
@@ -49,17 +49,9 @@ router.post("/", authentication, async (req, res, next) => {
   }
 })
 
-
-
 //update a swap
 router.put("/:id", authentication, async (req, res, next) => {
   try {
-    const updateWithId = req.params.id
-    const currentSwap = await Swap.findById(req.body._id)
-    const oneSwap = await Swap.findByIdAndUpdate(req.body._id,{
-        
-    })
-
     const foundUser = await User.findById(req.user._id)
     const foundSwap = await Swap.findById(req.params.id)
     console.log(foundUser)
@@ -73,14 +65,13 @@ router.put("/:id", authentication, async (req, res, next) => {
         const updatedSwap = await Swap.findByIdAndUpdate(req.params.id,{
             ...req.body,
             house1:foundSwap.house2,
-            house2:foundSwap.house1
+            house2:foundSwap.house1,
+            messagesPossible:true
         },{new:true})
         res.status(200).json(updatedSwap)
     }else{
         res.status(400).json({message:"unauthorized ! the user has to own one of the 2 houses"})
-    }
-
-    
+    }    
   } catch (err) {
       next(err)
   }
@@ -104,4 +95,5 @@ router.delete("/:id", authentication , async (req, res, next) => {
     next(err)
   }
 })
+
 module.exports = router
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,15 +1,27 @@
-const db = require('../db/index')
+//require('dotenv').config('../.env')
+require('../db/index')
 const mongoose = require('mongoose')
 const House = require('../models/House.model')
-require('dotenv').config()
+//require('dotenv').config()
 const data = require('./data')
+const User = require('../models/User.model')
+
 
 const seed = async () =>{
     try{
         //console.log(data)
         // const ans = await House.create(data)
+        await House.deleteMany()
+        const allUsers = await User.find({accessLevel: 'user'})
+        const adminUser = await User.find({accessLevel:'admin'})
         const ans = await House.insertMany(data)
-        console.log('------>ANS : ',ans)
+        await Promise.all(ans.map(async(house,i)=>{
+            if(i<4){
+                await User.findByIdAndUpdate(allUsers[0],{ $push:{ ownedHouses:house._id } })
+            }else{
+                await User.findByIdAndUpdate(adminUser[0],{ $push:{ ownedHouses:house._id } })
+            }
+        }))
         mongoose.connection.close()
     }catch(e){
         console.log('EROR : ',e)
